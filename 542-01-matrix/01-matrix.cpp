14e9f319faab585e6ff658d13c507f29c7e073df
kavitharaghuram <245830806+kavitharaghuram@users.noreply.github.com>
2026-05-20 13:46:47 +0530

    Time: 19 ms (54.74%) | Memory: 39 MB (20.52%) - LeetSync

---
+++ 542-01-matrix/01-matrix.cpp
@@ -0,0 +1,40 @@
+class Solution {
+public:
+    vector<vector<int>> updateMatrix(vector<vector<int>>& mat) {
+        int n=mat.size();
+        int m=mat[0].size();
+        vector<vector<int>>vis(n, vector<int>(m,0));
+        vector<vector<int>>dist(n, vector<int>(m,0));
+        queue<pair<pair<int, int>, int>> q;
+        for(int i=0; i<n; i++){
+            for(int j=0; j<m; j++){
+                if(mat[i][j]==0){
+                    q.push({{i, j}, 0});
+                    vis[i][j]=1;
+                }
+                else {
+                    vis[i][j]=0;
+                }
+            }
+        }
+        int dr[]={-1, 0, 1, 0};
+        int dc[]={0, -1, 0, 1};
+        while(!q.empty()){
+            int r=q.front().first.first;
+            int c=q.front().first.second;
+            int steps=q.front().second;
+            dist[r][c]=steps;
+            q.pop();
+            for(int i=0; i<4; i++){
+                int nr=r+dr[i];
+                int nc=c+dc[i];
+                if(nr<n && nr>=0 && nc<m && nc>=0 && !vis[nr][nc]){
+                    vis[nr][nc]=1;
+                    q.push({{nr, nc}, steps+1});
+                }
+            }
+
+        }
+        return dist;
+    }
+};
