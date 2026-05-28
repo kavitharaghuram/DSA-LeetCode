bea0ba23ab38bff69c5ae250ecde54a131b61199
kavitharaghuram <245830806+kavitharaghuram@users.noreply.github.com>
2026-05-28 17:56:19 +0530

    Time: 0 ms (100.00%) | Memory: 20.3 MB (83.14%) - LeetSync

---
+++ 45-jump-game-ii/jump-game-ii.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int jump(vector<int>& nums) {
+        int jumps=0;
+        int curr=0;
+        int maxidx=0;
+        for(int i=0; i<nums.size()-1; i++){
+            maxidx=max(maxidx, i+nums[i]);
+            if(i==curr){
+                jumps++;
+                curr=maxidx;
+            }
+        }
+        return jumps;
+    }
+};
