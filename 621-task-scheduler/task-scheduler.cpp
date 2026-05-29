ef8f22640a39fd1bb49a1098b164925f56a4a313
kavitharaghuram <245830806+kavitharaghuram@users.noreply.github.com>
2026-05-29 09:11:07 +0530

    Time: 0 ms (100.00%) | Memory: 38.2 MB (94.68%) - LeetSync

--- 621-task-scheduler/task-scheduler.cpp
+++ 621-task-scheduler/task-scheduler.cpp
@@ -5,27 +5,13 @@ public:
         for(char task: tasks){
             count[task-'A']++;
         }
-        priority_queue<int>maxHeap;
-        for(int cnt: count){
-            if(cnt>0) maxHeap.push(cnt);
-        }
-        int time=0;
-        queue<pair<int, int>> q;
-        while(!q.empty() || !maxHeap.empty()){
-            time++;
-            if(maxHeap.empty()){
-                time=q.front().second;
-            }
-            else {
-                int cnt=maxHeap.top()-1;
-                maxHeap.pop();
-                if(cnt>0)q.push({cnt, time+n});
-            }
-            if(!q.empty() && q.front().second==time){
-                maxHeap.push(q.front().first);
-                q.pop();
+        int maxFreq= *max_element(count.begin(), count.end());
+        int maxCount=0;
+        for(int cn: count){
+            if (cn==maxFreq){
+                maxCount++;
             }
         }
-        return time;
+        return max((int)tasks.size(), ((n+1)*(maxFreq-1)+maxCount));
     }
 };
