f51af4f4ce4af287de090909466d5ac6200b7818
kavitharaghuram <245830806+kavitharaghuram@users.noreply.github.com>
2026-06-03 08:07:27 +0530

    Time: 7 ms (6.22%) | Memory: 17 MB (39.22%) - LeetSync

--- 1036-rotting-oranges/rotting-oranges.cpp
+++ 1036-rotting-oranges/rotting-oranges.cpp
@@ -3,9 +3,9 @@ public:
     int orangesRotting(vector<vector<int>>& grid) {
         int n= grid.size();
         int m= grid[0].size();
-        queue<pair<pair<int, int>, int>> q;
-        vector<vector<int>> vis(n, vector<int>(m, 0));
-        int cntfresh=0;
+        vector<vector<int>>vis(n, vector<int>(m, 0));
+        queue<pair<pair<int, int>, int>>q;
+        int countfresh=0;
         for(int i=0; i<n; i++){
             for(int j=0; j<m; j++){
                 if(grid[i][j]==2){
@@ -15,31 +15,31 @@ public:
                 else {
                     vis[i][j]=0;
                 }
-                if( grid[i][j]==1)cntfresh++;
+                if(grid[i][j]==1)countfresh++;
             }
+
         }
         int time=0;
+        int count=0;
         int dr[]={-1, 0, 1, 0};
         int dc[]={0, -1, 0, 1};
-        int cnt=0;
         while(!q.empty()){
-            int r= q.front().first.first;
+            int r = q.front().first.first;
             int c= q.front().first.second;
             int t= q.front().second;
             q.pop();
-            time=max(time, t);
+            time= max(time, t);
             for(int i=0; i<4; i++){
                 int nr= r+dr[i];
                 int nc= c+dc[i];
-                if(nr>=0 && nr<n && nc>=0 && nc<m && vis[nr][nc]!=2 && grid[nr][nc]==1){
+                if(nr<n && nr>=0 && nc<m && nc>=0 && grid[nr][nc]==1 && vis[nr][nc]!=2 ){
                     q.push({{nr, nc}, t+1});
                     vis[nr][nc]=2;
-                    cnt++;
+                    count++;
                 }
-
             }
         }
-        if(cnt!=cntfresh)return -1;
-        else return time;
+        if(count!=countfresh)return -1;
+        return time;
     }
 };
