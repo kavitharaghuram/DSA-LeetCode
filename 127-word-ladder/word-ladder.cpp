3d42e2fd747e261df8ba8fc01ebe0f13f6a01b5a
kavitharaghuram <245830806+kavitharaghuram@users.noreply.github.com>
2026-06-06 09:04:09 +0530

    Time: 36 ms (94.73%) | Memory: 21.3 MB (42.96%) - LeetSync

--- 127-word-ladder/word-ladder.cpp
+++ 127-word-ladder/word-ladder.cpp
@@ -1,36 +1,26 @@
 class Solution {
 public:
     int ladderLength(string beginWord, string endWord, vector<string>& wordList) {
-        if(endWord.empty() || find(wordList.begin(), wordList.end(), endWord)== wordList.end())return 0;
-        unordered_map<string, vector<string>> nei;
-        wordList.push_back(beginWord);
-        for(const string& word: wordList){
-            for(int j=0; j< word.size(); j++){
-                string pattern = word.substr(0, j)+ '*' + word.substr(j+1);
-                nei[pattern].push_back(word); 
-            }
-        }
-        unordered_set<string> visit {beginWord};
-        queue<string> q;
-        q.push(beginWord);
-        int res=1;
+        unordered_set<string>st(wordList.begin(), wordList.end());
+        queue<pair<string, int>>q;
+        q.push({beginWord, 1});
+        st.erase(beginWord);
         while(!q.empty()){
-            int size=q.size();
-            for(int i=0; i<size; i++){
-                string word= q.front();
-                q.pop();
-                if(word==endWord)return res;
-                for(int j=0; j<word.size(); j++){
-                    string pattern = word.substr(0, j)+ '*' + word.substr(j+1);
-                    for (const string& neiword: nei[pattern]){
-                        if(!visit.count(neiword)){
-                            visit.insert(neiword);
-                            q.push(neiword);
-                        }
+            string word= q.front().first;
+            int steps= q.front().second;
+            q.pop();
+            if(word==endWord)return steps;
+            for(int i=0; i<word.size(); i++){
+                char original= word[i];
+                for(char ch='a'; ch<='z'; ch++){
+                    word[i]=ch;
+                    if(st.count(word)){
+                        q.push({word, steps+1});
+                        st.erase(word);
                     }
                 }
+                word[i]=original;
             }
-            res++;
         }
         return 0;
     }
