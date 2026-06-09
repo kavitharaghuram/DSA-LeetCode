1b9e88fedca3cfbc135ff9033719f7277b9317b9
kavitharaghuram <245830806+kavitharaghuram@users.noreply.github.com>
2026-06-09 10:18:50 +0530

    Time: 0 ms (100.00%) | Memory: 9.1 MB (8.46%) - LeetSync

---
+++ 58-length-of-last-word/length-of-last-word.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int lengthOfLastWord(string s) {
+        int count=0;
+        int tail=s.length()-1;
+        while(tail>=0 && s[tail]==' ')tail--;
+        while(tail>=0 && s[tail]!=' '){
+            count++;
+            tail--;
+        }
+        return count;
+    }
+};
