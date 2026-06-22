c94ba526b1d77ac0323d8070ad3dfac8a96b36da
kavitharaghuram <245830806+kavitharaghuram@users.noreply.github.com>
2026-06-22 11:18:36 +0400

    Time: 0 ms (100.00%) | Memory: 10.8 MB (21.87%) - LeetSync

--- 198-house-robber/house-robber.cpp
+++ 198-house-robber/house-robber.cpp
@@ -1,15 +1,15 @@
 class Solution {
 public:
-    int f(int idx, vector<int>& nums, vector<int>& dp){
-        if(idx==0)return nums[idx];
-        if(idx<0)return 0;
-        if(dp[idx]!=-1)return dp[idx];
-        int pick= nums[idx]+f(idx-2, nums, dp);
-        int notpick=0+f(idx-1, nums, dp);
-        return dp[idx]=max(pick, notpick);
-    }
     int rob(vector<int>& nums) {
-        vector<int>dp(nums.size()+1, -1);
-        return f(nums.size()-1, nums, dp);
+        vector<int>dp(nums.size());
+        if(nums.size()==1)return nums[0];
+        dp[0]=nums[0];
+        for(int i=1; i<nums.size(); i++){
+            int take= nums[i];
+            if(i>1)take+=dp[i-2];
+            int nontake= dp[i-1];
+            dp[i]=max(take, nontake);
+        }
+        return dp[nums.size()-1];
     }
 };
